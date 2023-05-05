e5cd3fe5d0f21c47ed83d5f600eb976e9846e839
Eye Patch <s.salehi1381@gmail.com>
2023-05-05 16:06:49 +0330

    Update translator.py

--- translator.py
+++ translator.py
@@ -3,7 +3,7 @@ import pandas as pd
 from googletrans import Translator, LANGUAGES
 
 def remove_extra_symbols(text):
-    text = re.sub(r'[^a-zA-Z]+', ' ', text)
+    text = re.sub(r'[^a-zA-Z|]+', ' ', text)
     return re.sub(r'\s+', ' ', text)
 
 def translate_text(text, translator):
@@ -29,12 +29,14 @@ def translate_dataframe_for_multiple_cell(df):
     translator = Translator()
     
     columns = '|||'.join(df.columns.tolist())
+    columns = remove_extra_symbols(columns)
     translated_columns = translate_text(columns, translator).split('|||')
 
     df.columns = translated_columns
 
     for column in df.columns:
         combined_text = '|||'.join(df[column].dropna())
+        combined_text = remove_extra_symbols(combined_text)
         translated_text = translate_text(combined_text, translator)
         translated_values = translated_text.split('|||')
         df.loc[df[column].notna(), column] = translated_values
@@ -43,7 +45,7 @@ def translate_dataframe_for_multiple_cell(df):
 def main():
     df = pd.read_csv('er.csv')
 
-    translated_df = translate_dataframe_foreach_cell(df)
+    translated_df = translate_dataframe_for_multiple_cell(df)
     translated_df.to_csv('ep12_fa.csv', index=False)
 
 if __name__ == "__main__":
